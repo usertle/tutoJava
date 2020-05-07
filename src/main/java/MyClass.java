dd474dfe046108a830f73eef504a6b79dafcde3c
‘usertle’ <usertle@yahoo.com>
2020-05-07 17:21:47 +0200

    2ème modifiction

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -2,6 +2,7 @@
 public class MyClass {
   public static void main(String[] args) {
     System.out.println("Hello le monde merveilleux");   // modification du titre
+    System.out.println("!!");   // modification du titre
   }
 }
 
